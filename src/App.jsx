24ee7738689cfcc7dc663bfd76ba10b1b4834022
Andre Aditya Amann <andreadityam@gmail.com>
2025-09-04 00:29:14 +0700

    update: add download cv button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
 import pp from './assets/profpic.jpg'
 import payplus from './assets/payplus.png'
 import veg from './assets/vegetable.png'
@@ -7,9 +6,8 @@ import int from './assets/interactive.png'
 import car from './assets/cardashboard.png'
 import car2 from './assets/careda.png'
 import cus from './assets/customer.png'
-import viteLogo from '/vite.svg'
 import { Button } from "@/components/ui/button"
-import { Mail, User, Code, BarChart3, Server, Smartphone, Database, ExternalLink } from 'lucide-react';
+import { File, Mail, User, Code, BarChart3, Server, Smartphone, Database, ExternalLink } from 'lucide-react';
 import {
   Card,
   CardContent,
@@ -68,6 +66,8 @@ const projects = [
   { name: 'Customer Purchase Behavior Exploratory Data Analysis', img: cus, link: 'https://github.com/andreadityam/customer-purchases-behavior-eda', description: 'Exploratory Data Analysis (EDA) of customer purchase behavior using Python. It explores demographics, income, spending habits, loyalty, and satisfaction to uncover patterns and insights. Based on a simulated dataset from Kaggle.', tech: ['Python', 'Data Analysis', 'Data Visualization', 'Pandas', 'Numpy', 'Matplotlib', 'Seaborn'], },
 ];
 
+const cvPath = `${import.meta.env.BASE_URL}/CV.pdf`;
+
 function App() {
   const [isScrolled, setIsScrolled] = useState(false);
   const [iconColor, setIconColor] = useState('#000000');
@@ -108,6 +108,7 @@ function App() {
         <p className='text-gray-950/70 mt-5 md:text-lg max-w-2xl mx-auto text-md'>A passionate Full-Stack Developer & Data Analyst dedicated to building innovative web applications and extracting actionable insights from data.</p>
         <div className='mt-5 px-25 flex flex-col sm:flex-row justify-center gap-4'>
           <a href='#contacts'><Button size='lg' className='hover:-translate-y-0.5 bg-gray-800 text-white rounded-lg cursor-pointer hover:bg-gray-800/90'><User />Contacts</Button></a>
+          <a href={cvPath} download='CV_Andre.pdf'><Button size='lg' className='hover:-translate-y-0.5 bg-blue-600 text-white rounded-lg cursor-pointer hover:bg-blue-800/90'><File />Download CV</Button></a>
           <a href='https://github.com/andreadityam' target='_blank'><Button onMouseEnter={() => setIconColor('white')} onMouseLeave={() => setIconColor('#000000')} size='lg' className='bg-white hover:-translate-y-0.5 border-gray-300 hover:bg-blue-500 hover:text-white cursor-pointer' variant="outline"><svg fill={iconColor} role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" /></svg>View GitHub</Button></a>
         </div>
       </section>
