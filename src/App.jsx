823dcf75c73c3564b5a2056c69d4ef1fef433306
Andre Aditya Amann <andreadityam@gmail.com>
2025-09-19 14:32:31 +0700

    update: new layout

--- src/App.jsx
+++ src/App.jsx
@@ -118,15 +118,19 @@ function App() {
           </ul>
         </nav>
       </div>
-      <section className='text-center p-8 pb-20'>
-        <img className='border-blue-300/40 border-3 mb-5 mx-auto md:h-45 h-35 rounded-full' src={pp}></img>
-        <h1 className='font-bold md:text-5xl text-gray-800 max-w-2xl mx-auto text-3xl'>Hello, I'm</h1>
-        <h1 className='pt-2 font-bold md:text-5xl max-w-2xl mx-auto text-3xl'> <span className='text-blue-700'>Andre Aditya Amann</span></h1>
-        <p className='text-gray-950/70 mt-5 md:text-lg max-w-2xl mx-auto text-md'>A passionate Full-Stack Developer & Data Analyst dedicated to building innovative web applications and extracting actionable insights from data.</p>
-        <div className='mt-5 px-7 sm:px-25 flex flex-col sm:flex-row justify-center gap-4'>
-          <a href='#contacts'><Button className='hover:-translate-y-0.5 w-full bg-gray-800 text-white rounded-lg cursor-pointer hover:bg-gray-800/90'><User />Contacts</Button></a>
-          <a href={cvPath} download='CV_Andre.pdf'><Button className='hover:-translate-y-0.5 w-full bg-blue-600 text-white rounded-lg cursor-pointer hover:bg-blue-800/90'><File />Download CV</Button></a>
-          <a href='https://github.com/andreadityam' target='_blank'><Button onMouseEnter={() => setIconColor('white')} onMouseLeave={() => setIconColor('#000000')} className='bg-white hover:-translate-y-0.5 w-full border-gray-300 hover:bg-blue-600 hover:text-white cursor-pointer' variant="outline"><svg fill={iconColor} role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" /></svg>View GitHub</Button></a>
+      <section className='text-center p-8 pb-20 md:flex md:justify-center md:items-center mt-10'>
+        <div className='mb-5 lg:mb-0'>
+          <img className='border-blue-300/40 border-3 mx-auto lg:h-60 h-45 rounded-full' src={pp}></img>
+        </div>
+        <div className='ml-5 lg:ml-10'>
+          <h1 className='font-bold lg:text-5xl text-gray-800 text-3xl md:text-left'>Hello, I'm</h1>
+          <h1 className='lg:pt-2 font-bold lg:text-5xl text-3xl md:text-left'> <span className='text-blue-700'>Andre Aditya Amann</span></h1>
+          <p className='text-gray-950/70 mt-3 lg:mt-5 mx-auto md:text-lg max-w-lg text-md md:text-justify leading-6 lg:leading-normal'>A passionate Full-Stack Developer & Data Analyst dedicated to building innovative web applications and extracting actionable insights from data.</p>
+          <div className='lg:mt-5 mt-4 flex flex-col sm:flex-row gap-4 justify-center md:justify-start'>
+            <a href='#contacts'><Button className='hover:-translate-y-0.5 w-full bg-gray-800 text-white rounded-lg cursor-pointer hover:bg-gray-800/90'><User />Contacts</Button></a>
+            <a href={cvPath} download='CV_Andre.pdf'><Button className='hover:-translate-y-0.5 w-full bg-blue-600 text-white rounded-lg cursor-pointer hover:bg-blue-800/90'><File />Download CV</Button></a>
+            <a href='https://github.com/andreadityam' target='_blank'><Button onMouseEnter={() => setIconColor('white')} onMouseLeave={() => setIconColor('#000000')} className='bg-white hover:-translate-y-0.5 w-full border-gray-300 hover:bg-blue-600 hover:text-white cursor-pointer' variant="outline"><svg fill={iconColor} role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" /></svg>View GitHub</Button></a>
+          </div>
         </div>
       </section>
       <section id='skills' className='text-center p-5 md:pt-15 lg:px-25 pb-25'>
